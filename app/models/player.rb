a68a9b96be003969f02de9ad6ca5c8ded70d2280
kor-Chipmunk <rhj4862@gmail.com>
2017-08-08 17:54:28 +0900

    Add Devise and Layout files

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,12 @@
 class Player < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+    
     belongs_to :theme
     belongs_to :team
-    
+
     has_many :solve_stages
     has_many :stages, through: :solve_stages
 
