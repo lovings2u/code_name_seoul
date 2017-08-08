a454ec66e995bc2954be6f57ec79777135c991f0
kor-Chipmunk <rhj4862@gmail.com>
2017-08-08 19:07:47 +0900

    Add Answer Check

--- app/controllers/stage_controller.rb
+++ app/controllers/stage_controller.rb
@@ -4,4 +4,23 @@ class StageController < ApplicationController
     def index
 
     end
+
+    def answercheck
+        stage = Stage.find(params[:stage_id])
+        answer = params[:answer]
+
+        respond_to do |format|
+            if stage.answer == answer
+                result = Hash.new
+                result["result"] = "P"
+                format.html { render xml: result.to_xml }
+                format.xml { render xml: result.to_xml }
+            else
+                result = Hash.new
+                result["result"] = "F"
+                format.html { render xml: result.to_xml }
+                format.xml { render xml: result.to_xml }
+            end
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :players
   root 'stage#index'
+  get '/answer/:stage_id/:answer' => 'stage#answercheck'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
